1f99e9a7cd09f7d78866a72b0908d4d6dcb44d70
kouki.0109721 <kouki.0109721@au.com>
2022-05-09 10:33:45 +0900

    UE5プロジェクト追加

---
+++ ImportModelPlugin/Intermediate/Build/Win64/ImportModelPluginEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.31.31107
+#include "C:/Users/koki.yamaguchi/Documents/UE5Training/ImportModelPlugin/Intermediate/Build/Win64/ImportModelPluginEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ImportModelPlugin/Source/ImportModelPlugin/ImportModelPluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ImportModelPluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IMPORTMODELPLUGIN_API AImportModelPluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
